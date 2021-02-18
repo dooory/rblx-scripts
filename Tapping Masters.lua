bf731693972793a12e3bdb92a7af60321a78d4f5
Dory <78720364+dooory@users.noreply.github.com>
2021-02-18 16:31:46 +1300

    Changed AGAIN

--- Tapping Masters.lua
+++ Tapping Masters.lua
@@ -1,6 +1,6 @@
 local flags = {enabled = false}
 
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/dooory/rblx-scripts/main/edited-turtle-ui.lua?token=ASYS23AMVNKZXH57GRWGI7DAFXJ72"))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/dooory/rblx-scripts/main/edited-turtle-ui.lua"))()
 local window = library:window("Tapping Masters")
 
 window:label("Made by dory")
